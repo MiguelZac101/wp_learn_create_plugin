a2f8d4ae3f10ca318e0bbf3e8ab7ae982dbdb215
zack <miguel.quispe@fractal.com.pe>
2025-07-22 22:38:27 -0500

    74. Funciones para el uso de las peticiones (Parte 2)

--- includes/mp-http.php
+++ includes/mp-http.php
@@ -2,7 +2,10 @@
 class MP_Http {
     public function show(){
         echo "MP_Http<br>";
-        $response = wp_remote_get( 'https://api.github.com/users/wordpress' );
+        //$response = wp_remote_get( 'https://api.github.com/users/wordpress' );
+        //con objeto
+        $http = new WP_Http();
+        $response = $http->get('https://api.github.com/users/wordpress');
 
         //$response = wp_remote_retrieve_body( $response );// selecciona solo el body
 
@@ -11,6 +14,10 @@ class MP_Http {
         //echo "<pre>";
         var_dump($response);
         //echo "</pre>";
+
+        //funciones para obtener encabezados
+        //wp_remote_retrieve_headers( array $response ) //todos los headers
+        //wp_remote_retrieve_header( array $response, string $header ) // un dato del header ejm 'last-modified'
     }
 }
 
